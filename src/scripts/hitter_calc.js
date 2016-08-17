b77274434b9864208317382c2bfceb19f59eacab
Alan C <alanc@kidney.org>
2016-08-17 15:29:29 -0400

    Refined Javascript Code

--- src/scripts/hitter_calc.js
+++ src/scripts/hitter_calc.js
@@ -5,25 +5,37 @@ var $ = function (id) {
 function calculate_stats () 
 {
     G = $("games").value;
-    AB = $("atBats").value;
-    H = $("hits").value;
-    R = $("runs").value;
-    DB = $("doubles").value;
-    TB = $("triples").value;
-    HR = $("homeruns").value;
-    RBI = $("rbi").value;
-    K = $("strikeouts").value;
-    BB = $("walks").value; 
-    HBP = $("hbp").value; 
-    SF = $("sf").value; 
+    AB =$("atBats").value;
+     H = $("hits").value;
+     R = $("runs").value;
+     DB = $("doubles").value;
+     TB = $("triples").value;
+     HR = $("homeruns").value;
+     RBI = $("rbi").value;
+     SB = $("sb").value;
+     K = $("strikeouts").value;
+     BB = $("walks").value; 
+     HBP = $("hbp").value; 
+     SF = $("sf").value; 
 
-    var BA_raw = H / AB;
+    //Calculate Batting Average
+    var BA_raw = parseInt(H) / parseInt(AB);
     var BA = Math.ceil(BA_raw *1000) / 1000;
 
-    var OBP_raw = (H + BB + HBP) / (AB + BB + HBP + SF);
+    //Calculate On Base Percentage
+    var OBP_raw = (parseInt(H) + parseInt(BB) + parseInt(HBP)) / (parseInt(AB) + parseInt(BB) + parseInt(HBP) + parseInt(SF));
     var OBP = Math.ceil(OBP_raw *1000) / 1000;
 
-    alert(BA, OBP);
+    //Calculate Slugging Percentage
+    var S = (parseint(H) - parseInt(DB) - parseInt(TB) - parseInt(HR));
+    var TOTB = S + (2 * parseInt(DB)) + (3 * parseInt(TB)) + (4 * parseInt(HR));
+    var SLG_raw = paraeInt(TOTB) / parseInt(AB);
+    var SLG = Math.ceil(SLG_raw *1000) / 1000;
+
+    //Calculate OPS
+    var OPS = parseInt(OBP) + parseInt(SLG);
+
+    alert(OBP);
    
     
 }
