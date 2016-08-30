a7fca74c7f7af0700bae730d6e385be8241c177e
Alan C <alanc@kidney.org>
2016-08-30 15:56:11 -0400

    Simplified Equations

--- src/scripts/hitter_calc.js
+++ src/scripts/hitter_calc.js
@@ -7,36 +7,37 @@ function calculate_stats ()
 
      document.getElementById('results').classList.add('show');
 
-     G = $("games").value;
-     AB =$("atBats").value;
-     H = $("hits").value;
-     R = $("runs").value;
-     DB = $("doubles").value;
-     TB = $("triples").value;
-     HR = $("homeruns").value;
-     RBI = $("rbi").value;
-     SB = $("sb").value;
-     K = $("strikeouts").value;
-     BB = $("walks").value; 
-     HBP = $("hbp").value; 
-     SF = $("sf").value; 
+     G = +$("games").value;
+     AB = +$("atBats").value;
+     H = +$("hits").value;
+     R = +$("runs").value;
+     DB = +$("doubles").value;
+     TB = +$("triples").value;
+     HR = +$("homeruns").value;
+     RBI = +$("rbi").value;
+     SB = +$("sb").value;
+     K = +$("strikeouts").value;
+     BB = +$("walks").value; 
+     HBP = +$("hbp").value; 
+     SF = +$("sf").value; 
 
     //Calculate Batting Average
-    BA_raw = parseInt(H) / parseInt(AB);
+    BA_raw = H / AB;
     BA = Math.ceil(BA_raw *1000) / 1000;
 
     //Calculate On Base Percentage
-    var OBP_raw = (parseInt(H) + parseInt(BB) + parseInt(HBP)) / (parseInt(AB) + parseInt(BB) + parseInt(HBP) + parseInt(SF));
+    var OBP_raw = (H + BB + HBP) / (AB + BB + HBP + SF);
     var OBP = Math.ceil(OBP_raw *1000) / 1000;
 
     //Calculate Slugging Percentage
-    var S = (parseInt(H) - parseInt(DB) - parseInt(TB) - parseInt(HR));
-    var TOTB = S + (2 * parseInt(DB)) + (3 * parseInt(TB)) + (4 * parseInt(HR));
-    var SLG_raw = parseInt(TOTB) / parseInt(AB);
+    var S = (H - DB - TB - HR);
+    var TOTB = S + (2 * DB) + (3 * TB) + (4 * HR);
+    var SLG_raw = TOTB / AB;
     var SLG = Math.ceil(SLG_raw *1000) / 1000;
 
     //Calculate OPS
-    var OPS = OBP + SLG;
+    var OPS_raw = OBP + SLG;
+    var OPS = Math.ceil(OPS_raw *1000) / 1000;
 
     $("G_Out").innerHTML = G;
     $("AB_Out").innerHTML = AB;
